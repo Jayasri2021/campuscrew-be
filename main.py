00753c76d16194f317da68daab44d90d55dd73a8
pfw_Jayasri_Suresh_Vani <102977678+Jayasri2021@users.noreply.github.com>
2024-12-06 12:55:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ app = create_app()
 CORS(app)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))  # Changed default to 8080
     app.run(host='0.0.0.0', port=port, debug=False)
